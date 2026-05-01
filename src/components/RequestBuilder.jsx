cb491b36fc6fcc291ae159d088895e7bacfed64d
MATRUNI <h.uyadav2004@gmail.com>
2026-05-01 23:06:13 +0530

    implemented the logic for api call

--- src/components/RequestBuilder.jsx
+++ src/components/RequestBuilder.jsx
@@ -4,14 +4,52 @@ import Body_panel from '../request-panel/Body_panel'
 import Header_panel from '../request-panel/Header_panel'
 import Query_panel from '../request-panel/Query_panel'
 import { RequestContext } from '../context/RequestContext'
+import { callAPI } from '../services/api'
 
 function RequestBuilder() {
-    const {url,setURL}=useContext(RequestContext)
+    const {url,setURL,request,setResponse}=useContext(RequestContext)
     const [method,setMethod]=useState("GET")
     const [activeTab,setActiveTab]=useState('body')
+    const isValidURL=(value)=>
+    {
+      try{
+        new URL(value)
+        return true;
+      }
+      catch
+      {
+        return false;
+      }
+    }
+    const handleSubmit=async(e)=>
+    {
+      e.preventDefault();
+      console.log("URl: ",url)
+      console.log("method: ",method)
+      if(!isValidURL(url))
+      {
+        alert("Invalid URL");
+        return;
+      }
+      try{
+        const response=await callAPI(url, method, request);
+        setResponse({
+          status:response.status,
+          data:response.data,
+          time:response.time,
+        })
+      }
+      catch(error)
+      {
+        setResponse({
+          status:error.status,
+          data:error.message,
+        })
+      }
+    }
   return (
 <section className="pane request-pane">
-      <div className="url-bar-group">
+      <form className="url-bar-group" onSubmit={handleSubmit}>
         <select 
           className={`method-dropdown method-${method}`}
           value={method} 
@@ -21,9 +59,9 @@ function RequestBuilder() {
           <option value="PUT">PUT</option>
           <option value="DELETE">DELETE</option>
         </select>
-        <input type="text" className="url-input" value={url}/>
-        <button className="send-button">Send</button>
-      </div>
+        <input type="text" className="url-input" value={url} onChange={(e)=>setURL(e.target.value)}/>
+        <button className="send-button" type='submit'>Send</button>
+      </form>
       <Tabs activeTab={activeTab} setActiveTab={setActiveTab} />
 
       {activeTab === "body" && <Body_panel/>}

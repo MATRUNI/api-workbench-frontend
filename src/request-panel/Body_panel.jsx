f33448dfc66e9b64197c4dbda611751ead51dad4
MATRUNI <h.uyadav2004@gmail.com>
2026-05-02 17:30:03 +0530

    replace pre with textarea, added json button for json format

--- src/request-panel/Body_panel.jsx
+++ src/request-panel/Body_panel.jsx
@@ -1,30 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import { RequestContext } from '../context/RequestContext';
 
 function Body_panel() {
     const {request,setRequest} =useContext(RequestContext)
+    const [localString,setLocalString]=useState(JSON.stringify(request.body||{},null,2))
     function handleInput(e)
     {
-        let value=e.target.textContent;
-        setRequest(JSON.parse(value||{
-              "key": "value",
-              "data": "input your JSON here"
-            }))
+        setLocalString(e.target.value)
+    }
+    function handleSync()
+    {
+      const parsed=JSON.parse(localString);
+      setRequest(prev=>({
+        ...prev,
+        body:parsed
+      }))
+      setLocalString(JSON.stringify(parsed,null,2))
     }
   return (
   <div className="editor-area">
       <div className="pane-header">
         <span className="label">JSON Body</span>
+        <button onClick={handleSync}>JSON</button>
       </div>
       <div className="editor-window">
-        <pre 
-          className="code-input"
-          contentEditable="true" 
-          suppressContentEditableWarning={true}
-          onInput={e => handleInput(e)}
+        <textarea 
+          className="code-input ghost-textarea"
+          onChange={handleInput}
+          onBlur={handleSync}
+          value={localString}
+          spellCheck='false'
+          autoCapitalize='none'
         >
-          {JSON.stringify(request.body, null, 2)}
-        </pre>
+        </textarea>
       </div>
     </div>
   )

b9cb8316828a8163cae2261819ab455a10260be4
MATRUNI <h.uyadav2004@gmail.com>
2026-05-19 23:22:13 +0530

    fix: typo bug

--- src/services/ArrayToObject.js
+++ src/services/ArrayToObject.js
@@ -1,7 +1,7 @@
 export function ArrayToObject(arr=[])
 {
     arr.reduce((acc,item)=>{
-        if(item.key) acc[key]=item.value
+        if(item.key) acc[item.key]=item.value
         return acc;
     },{});
 }
